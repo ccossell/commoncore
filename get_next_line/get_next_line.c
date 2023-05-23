74573cc72e0a4567a8e6917985b4f65d5961a86d
ClaudioCossellu <75547629+ccossell@users.noreply.github.com>
2023-05-23 19:19:56 +0200

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 18:51:17 by marvin            #+#    #+#             */
+/*   Updated: 2023/05/23 18:51:17 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    *ft_read_to_str(int fd, char *str)
+{
+    char    *buff;
+    int     rd_bytes;
+
+    buff = (char *)malloc(BUFFER_SIZE + 1);
+    if (!buff)
+        return (NULL);
+    rd_bytes = 1;
+    while (!ft_strchr(str, '\n') && rd_bytes != 0)
+    {
+        rd_bytes = read(fd, buff, BUFFER_SIZE);
+        if (rd_bytes = -1)
+        {
+            free(buff);
+            return (NULL);
+        }
+        buff[rd_bytes] = '\0';
+        str = ft_strjoin(str, buff);
+    }
+    free(buff);
+    return (str);
+}
+
+char    *get_next_line(int fd)
+{
+    char        *line;
+    static char *str;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    str = ft_read_to_str(fd, str);
+    if (!str)
+        return (NULL);
+    line = ft_get_line(str);
+    str = ft_new_str (str);
+    return (line);
+}
