0eebbfc285e4def6888a78ab4ae68b56e7d0dccf
ClaudioCossellu <75547629+ccossell@users.noreply.github.com>
2023-05-23 18:50:17 +0200

    Add files via upload

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 17:16:47 by marvin            #+#    #+#             */
+/*   Updated: 2023/05/23 17:16:47 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    ft_get_line(char *str)
+{
+    int     i;
+    char    *strm;
+
+    i = 0;
+    if (!str[i])
+        return (NULL);
+    while (str[i] && str[i] != '\n')
+        i++;
+    strm = (char *)malloc(i + 2);
+    if (!strm)
+        return (NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+    {
+        strm[i] = str[i];
+        i++;
+    }
+    if (str[i] == '\n')
+    {
+        strm[i] = str[i];
+        i++;
+    }
+    strm[i] = '\0';
+    return (strm);
+}
+
+char    ft_new_str(char *str)
+{
+    int     i;
+    int     j;
+    char    *strm;
+
+    i = 0;
+    while (str[i] && str[i] != '\n')
+        i++;
+    if (!str)
+    {
+        free(str);
+        return (NULL);
+    }
+    strm = (char *)malloc(ft_strlen(str) - i + 1);
+    if (!strm)
+        return (NULL);
+    i++;
+    j = 0;
+    while (str[j])
+        strm[j++] = str[i++];
+    strm[j] = '\0';
+    free(str);
+    return (strm);
+}
