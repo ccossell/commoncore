a1755e0a92512e6d2eeec18f05a3246eef4de605
ClaudioCossellu <75547629+ccossell@users.noreply.github.com>
2023-12-07 13:32:03 +0100

    Update minitalk.h

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccossell <ccossell@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: ccossell <ccossell@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 11:10:18 by ccossell            #+#    #+#             */
 /*   Updated: 2023/03/08 14:44:51 by ccossell           ###   ########.fr       */
